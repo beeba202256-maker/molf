927578eb7a302cb0f629ce3f27d25cfb0c3c121f
naman <naman@users.noreply.github.com>
2026-04-20 22:32:36 -0700

    feat: serve frontend build from backend root

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const compression = require("compression");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const path = require("path");
+const fs = require("fs");
 const apiRateLimit = require("./middlewares/rateLimit.middleware");
 const errorMiddleware = require("./middlewares/error.middleware");
 const routes = require("./routes");
@@ -17,6 +19,14 @@ app.use(express.json({ limit: "1mb" }));
 app.use(morgan("dev"));
 
 app.use("/api", routes);
+
+const frontendDistPath = path.resolve(__dirname, "../../frontend/dist");
+if (fs.existsSync(frontendDistPath)) {
+  app.use(express.static(frontendDistPath));
+  app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(frontendDistPath, "index.html"));
+  });
+}
 
 app.use((req, res) => {
   res.status(404).json({
